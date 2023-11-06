0b637d6f98ab252bcb941adf835912c2c5ac6668
sontomtimde <mtimdesonto@yahoo.com>
2023-11-06 04:45:52 -0800

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for element in my_string:
+        if element.lower() != "c":
+            new_string += element
+    return new_string
