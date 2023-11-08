96401e5f599bf86969149674d7372e978b4de422
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 10:15:44 -0800

    simply_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,9 +3,5 @@ def simple_delete(a_dictionary, key=""):
     if not isinstance(a_dictionary, dict):
         print("Error: Input must be a dictionary.")
         return
-    if key in a_dictionary:
-        a_dictionary.pop(key)
-        print(f"Key '{key}' deleted.")
-    else:
-        print(f"Key '{key}' not found in the dictionary.")
+    a_dictionary.pop(key, None)
     return a_dictionary
