14c675e37cab8424c27e52d65d7973e348ca369d
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 10:36:56 -0800

    -multiply_by_2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,2 @@
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if not isinstance(a_dictionary, dict):
-        raise TypeError("Input must be a dictionary.")
-    return {k: v * 2 if isinstance(v, (int, float)) else v for k, v in a_dictionary.items()}
+    return {k: v * 2 for k, v in a_dictionary.items() if isinstance(v, (int, float))}
