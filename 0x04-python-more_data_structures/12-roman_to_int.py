8eef0861773d487670650afdad90c18a28f2ec27
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 11:46:03 -0800

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
-        print("Error: Invalid input. Please provide a valid Roman numeral string.")
-        return None
+    roman_string = roman_string.upper()
     # Rest of the code remains unchanged
