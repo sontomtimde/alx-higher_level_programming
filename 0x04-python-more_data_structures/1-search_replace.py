5561465eb8f95beb990ead0a2ddcb39627b101ec
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 08:30:23 -0800

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    if search in my_list:
-        return [replace if num == search else num for num in my_list]
-    else:
-        print(f"{search} not found in the list.")
-        return my_list
+    return [replace if num == search else num for num in my_list]
