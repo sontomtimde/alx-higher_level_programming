191cf5139a11fe9650a064c4d3fe11544e9355d6
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 12:01:00 -0800

    square_matrix_map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if not all(isinstance(row, list) and all(isinstance(i, (int, float)) for i in row) for row in matrix):
-        print("Error: Invalid input. Please provide a matrix with numeric elements.")
-        return None
     return list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
