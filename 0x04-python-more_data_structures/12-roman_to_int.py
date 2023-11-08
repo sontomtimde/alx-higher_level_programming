c1a566a51bbbe01d505842b304089b6a7da321df
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 11:36:18 -0800

    roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        print("Error: Invalid input. Please provide a valid Roman numeral string.")
+        return None
+    # Rest of the code remains unchanged
