4cb3f88a8b7fcc1f16085bae41daa9759f3c80e3
sontomtimde <mtimdesonto@yahoo.com>
2023-11-08 11:13:04 -0800

    multiply_list_map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    if not my_list:
+    try:
+        return list(map(lambda i: i * number, filter(lambda x: isinstance(x, (int, float)), my_list)))
+    except TypeError:
+        print("Error: Non-numeric elements found in the list.")
         return []
-    return list(map(lambda i: i * number, my_list))
