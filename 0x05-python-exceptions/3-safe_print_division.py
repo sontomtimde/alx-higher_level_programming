787be04d38ffea85051f7762f7ec5afde33bfbb1
sontomtimde <mtimdesonto@yahoo.com>
2023-11-22 14:32:40 -0800

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+        print
+    except TypeError:
+        div = None
+        print
+    finally:
+        print(f)
+    return div
