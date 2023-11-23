6af8c2b1da83e17f6b637f1b76fde7777ac4c95c
sontomtimde <mtimdesonto@yahoo.com>
2023-11-22 17:14:11 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Define the square as per 2-square.py"""
+"""define the square as per 2-square.py"""
 
 
 class Square:
-    """Defining the square by size"""
+    """defining the square by size"""
     def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        """Get the size of the square"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Set the size of the square"""
-        if not isinstance(value, int):
+        self.__size = size
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
 
-    @property
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
     def area(self):
-        """Calculate the area of the square"""
-        return self.__size ** 2
+        """returns the area"""
+        area = self.__size * self.__size
+        return area
