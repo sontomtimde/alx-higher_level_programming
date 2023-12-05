bb8fcd79c2b6df4dfe72c42b900c3726a24a45b7
sontomtimde <mtimdesonto@yahoo.com>
2023-12-05 09:41:06 -0800

    python files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defining the inherited list of class MyList."""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Developing sorted printing for built-in list class."""
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """To print the list sorted in ascending order."""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Defining the base geometry, class BaseGeometry."""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Base geometry."""
+    """Represent base geometry."""
 
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate the parameter."""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
